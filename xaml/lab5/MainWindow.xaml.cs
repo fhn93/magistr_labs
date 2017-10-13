9831817823788280d42315520c6de981b0182cc0
fhn93 <fhn93@inbox.ru>
2017-10-13 12:50:50 +0300

    лабы

---
+++ xaml/lab5/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PersonsList.ItemsSource = new List<string> { "John", "Mary", "Andy" };
+            ColorList.ItemsSource = new List<string> { "Red", "Green", "Yellow" };
+            SizeList.ItemsSource = new List<string> { "10", "20", "30" };         
+        }
+           
+    }
+}
