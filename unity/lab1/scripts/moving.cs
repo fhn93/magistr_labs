3a40635bc5456fa3780485fe87fe0b10bcee9406
fhn93 <fhn93@inbox.ru>
2017-10-23 19:01:55 +0300

    unity(1 not omplit)

---
+++ unity/lab1/scripts/moving.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour {
+	private const int MnS = 3;//множитель скорости
+	private const int MaxFlavers = 10;//максимальное колиетво цветков на экране
+	private Rigidbody body;//"персонаж"
+	private object[] ArrayFlovers;
+	// Use this for initialization
+	void Start () {
+		body=GetComponent<Rigidbody>();//наследуем свойства "персонжа"
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FixUpdate();
+		if (Input.GetKey("escape"))//выход из игры
+			Application.Quit();		
+	}
+
+	void FixUpdate(){
+		
+		float hor = Input.GetAxis ("Horizontal");//берем компоненты вектора силы
+		float ver = Input.GetAxis("Vertical");
+		Vector3 VC = new Vector3(hor,0,ver);//формируем вектор силы
+	
+		body.AddForce(VC*MnS);//действие силы на "персонажа"
+	}
+}
---
+++ unity/lab1/scripts/new_flover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class new_flover : MonoBehaviour {
+	public Transform flover1;
+
+	// Use this for initialization
+	void Start () {
+		for (var y = 0; y < 5; y++) {
+			for (var x = 0; x < 5; x++) {
+				Instantiate (flover1, new Vector3 (x, y, 0), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
