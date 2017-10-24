43d48ddcc55bdda3f30f28af9afa0398bd664959
fhn93 <fhn93@inbox.ru>
2017-10-25 00:37:09 +0300

    unity complit lab

--- unity/lab1/scripts/moving.cs
+++ unity/lab1/scripts/moving.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class moving : MonoBehaviour {
 	private const int MnS = 3;//множитель скорости
 	private const int MaxFlavers = 10;//максимальное колиетво цветков на экране
 	private Rigidbody body;//"персонаж"
-	private object[] ArrayFlovers;
+	private int score=0;
+
+	public Text TS;
+	public Transform cub;
+
+
 	// Use this for initialization
+	void create()
+	{
+		Instantiate(cub,new Vector3(Random.Range(-4.5f,4.5f),0.0f,Random.Range(-4.5f,4.5f)),Quaternion.identity);
+	}
+
 	void Start () {
 		body=GetComponent<Rigidbody>();//наследуем свойства "персонжа"
 	}
@@ -19,12 +30,24 @@ public class moving : MonoBehaviour {
 			Application.Quit();		
 	}
 
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.CompareTag("candy"))
+			{
+				Destroy(col.gameObject);
+			score++;
+			create ();
+			TS.text = "SCORE:   " + score;
+			}
+	}
+
+
 	void FixUpdate(){
 		
 		float hor = Input.GetAxis ("Horizontal");//берем компоненты вектора силы
 		float ver = Input.GetAxis("Vertical");
 		Vector3 VC = new Vector3(hor,0,ver);//формируем вектор силы
-	
+
 		body.AddForce(VC*MnS);//действие силы на "персонажа"
 	}
 }
